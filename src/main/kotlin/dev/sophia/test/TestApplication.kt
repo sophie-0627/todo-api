52f7e8afd18ae412d2c0f6b2a0330ccf2e7f869d
Sophia_quq <sofia0627sofia@gmail.com>
2026-02-04 13:35:52 +0800

    push3

--- src/main/kotlin/dev/sophia/test/TestApplication.kt
+++ src/main/kotlin/dev/sophia/test/TestApplication.kt
@@ -1,13 +1,24 @@
 package dev.sophia.test
 
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.OpenAPI
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 class TestApplication
 
 fun main(args: Array<String>) {
 	runApplication<TestApplication>(*args)
+}
+
+@Configuration
+class SwaggerConfig {
+	@Bean
+	fun customOpenAPI(): OpenAPI {
+		return OpenAPI()
+			.info(Info().title("Sample API"))
+	}
 }
